09e2090a80ac06012a401b6441462e6bfdee77f2
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2020-07-23 15:57:58 +0600

    decriment complete but stay bug

--- js/coustom.js
+++ js/coustom.js
@@ -1,33 +1,32 @@
+
 //disabled all text area.
 var length = document.querySelectorAll(".input").length;
-for(var i = 0; i<length; i++){
-   document.querySelectorAll(".input")[i].disabled = true;
+for (var i = 0; i < length; i++) {
+    document.querySelectorAll(".input")[i].disabled = true;
 }
-
 
 //add event listener in plus button (Incriment Number)
-function incrimentNumber(pluseButtonID,iphoneCountValue){
+function incrimentNumber(pluseButtonID, iphoneCountValue) {
     var count = 0;
-var countButton = document.getElementById(pluseButtonID);
-countButton.onclick = function(){
-    count++;
-    var ab = document.getElementById(iphoneCountValue).value = count;
-    console.log(ab);
-}
+    var countButton = document.getElementById(pluseButtonID);//find button
+    countButton.onclick = function () {//add onclick listener with btn
+        count++;
+        document.getElementById(iphoneCountValue).value = count; //replace value
+    }
 }
 
-incrimentNumber("iphoneCountBtnPlus","iphoneCount"); // call for iphone
-incrimentNumber("coverCountBtnPlus","coverCount"); // call for cover
+incrimentNumber("iphoneCountBtnPlus", "iphoneCount"); // call for iphone
+incrimentNumber("coverCountBtnPlus", "coverCount"); // call for cover
 
 //add event listener in plus button (Decrement Number)
-function decrementNumber(minusButtonID,iphoneCountLessValue){
-    var count = 0;
-var countButton = document.getElementById(minusButtonID);
-countButton.onclick = function(){
-    count--;
-    var ab = document.getElementById(iphoneCountLessValue).value = count;
-    console.log(ab);
+function decrementNumber(minusButtonID, iphoneCountLessValue) {
+    var count = 0;//get value from input block
+
+    var countButton = document.getElementById(minusButtonID); //find button
+    countButton.onclick = function () { //add onclick listener with btn
+        count--;
+        document.getElementById(iphoneCountLessValue).value = count;
+    }
 }
-}
-decrementNumber("iphoneCountBtnMinus","iphoneCount"); //call for Iphone
-decrementNumber("coverCountBtnMinus","coverCount"); // call for cover
+decrementNumber("iphoneCountBtnMinus", "iphoneCount"); //call for Iphone
+decrementNumber("coverCountBtnMinus", "coverCount"); // call for cover
